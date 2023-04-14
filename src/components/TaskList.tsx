0e2b68fc1b508e0b7e45bee16f0728e65bd2a65d
work\leonardo-pastre <tp.l.poliveira@d0007azurecloudtotvscom.onmicrosoft.com>
2023-04-14 16:08:41 -0300

    Resolução do desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,43 @@ interface Task {
   isComplete: boolean;
 }
 
+
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
-
+  
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.trim() === ''){
+      return
+    }
+
+    const newTask = {
+      id: Math.floor(Math.random() * 100) + 1,
+      title: newTaskTitle,
+      isComplete: false
+    }
+    
+    const newTasks = [...tasks, newTask]
+    setTasks(newTasks);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if(task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    })
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
   }
 
   return (
